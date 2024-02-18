df41cfb214174d11c337329c2f4b9aa1e487d00c
clintonpalmer <156226966+clintonpalmer@users.noreply.github.com>
2024-02-17 21:08:13 -0600

    2nd attempt - made progress

--- Prob2_EX1.py
+++ Prob2_EX1.py
@@ -1,30 +1,60 @@
 from Prob1_EX1 import statistics
+from hw3b import distribution
+import math
+
+num_cycles = 11
+tot_per_sample = 100
 
 #Supplier A
 def supplier_A():
-    num_cycles = 11
-    tot_per_sample = 100
-    mean = (1 + 0.375) / 2
-    std_dev = (1 - 0.375) / 6
     # Size range of rocks
-    lower_bound = 0.375
-    upper_bound = 1
+    lower_bound = .375
+    upper_bound = 1.0
+    mean = (upper_bound + lower_bound) / 2
+    std_dev = (upper_bound - lower_bound) / 6
     results, mean_of_sampling_means, variance_of_sampling_means = statistics(mean, std_dev, num_cycles, tot_per_sample, lower_bound, upper_bound)
-    print(f"Supplier A:\n Mean of sampling means: {mean_of_sampling_means}\n Variance of Sampling Means: {variance_of_sampling_means}")
+    print(f"Supplier A:\n Mean of sampling means: {mean_of_sampling_means:.6f}\n Variance of Sampling Means: {variance_of_sampling_means:.6f}")
     return mean_of_sampling_means, variance_of_sampling_means
 
-supplier_A()
+#supplier_A() # for debugging
 
 def supplier_B():
-    num_cycles = 11
-    tot_per_sample = 100
     lower_bound = 0
     upper_bound = 0.875
     mean = (upper_bound + lower_bound) / 2
     std_dev = (upper_bound - lower_bound) / 6
 
     results, mean_of_sampling_means, variance_of_sampling_means = statistics(mean, std_dev, num_cycles, tot_per_sample, lower_bound, upper_bound)
-    print(f"Supplier B:\n Mean of sampling means: {mean_of_sampling_means}\n Variance of Sampling Means: {variance_of_sampling_means}")
+    print(f"Supplier B:\n Mean of sampling means: {mean_of_sampling_means:.6f}\n Variance of Sampling Means: {variance_of_sampling_means:.6f}")
     return mean_of_sampling_means, variance_of_sampling_means
 
-supplier_B()
+#supplier_B()  # for debugging
+
+def stat_analysis(num_cycles):
+    mean_A, var_A = supplier_A()
+    mean_B, var_B = supplier_B()
+    # pooled_se = pooled_std_error(var_A, var_B, num_cycles, num_cycles)
+
+    # Calculate the t-statistic
+    Sp = math.sqrt((99) * mean_A ** 2 + (99) * mean_B ** 2) / 198
+    t_stat = (mean_A - mean_B)  / (Sp * math.sqrt(1 / 100 + 1 / 100))
+    print (f"t_stat = {t_stat}")
+    # Calculate the degrees of freedom
+    df = num_cycles - 1
+
+    # Calculate the p-value
+    p_val = 1- distribution(df, (t_stat))
+    print(f"Probability = {p_val}")
+
+    # Check if the null hypothesis is rejected or accepted
+    alpha = 0.05
+    #degrees of freedom = 10
+    # critical value c or z = 1.81
+    if p_val < alpha:
+        print("We reject the null hypothesis. Supplier B produces statistically significantly smaller numbers than Supplier A.")
+    else:
+        print("We accept the null hypothesis. Supplier B does not produce statistically significantly smaller numbers than Supplier A.")
+
+stat_analysis(num_cycles)
+
+
